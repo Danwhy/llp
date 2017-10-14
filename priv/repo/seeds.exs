1d065fb46073e4ab0f5bd356ed1b871c589c32fb
Danielwhyte <danwhy@gmail.com>
2017-10-14 16:09:26 +0100

    adds seed script for radicals

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,22 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias NimbleCSV.RFC4180, as: CSV
+alias Llp.Repo
+alias Llp.Characters.Radical
+
+"#{File.cwd!}/radicals.csv"
+|> File.read!
+|> CSV.parse_string
+|> Enum.each(fn [id, radical, stroke_count, onyomi, kunyomi, meaning] ->
+  %Radical{}
+  |> Radical.changeset(
+    %{
+      "radical" => radical, "stroke_count" => stroke_count,
+      "onyomi" => String.split(onyomi, ","),
+      "kunyomi" => String.split(kunyomi, ",")
+    }
+  )
+  |> Repo.insert!()
+end)
