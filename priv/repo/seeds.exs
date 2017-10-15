77bb72cc4d72475660808ee85196e4e792573bf6
Danielwhyte <danwhy@gmail.com>
2017-10-15 14:31:38 +0100

    adds seed script for kanji

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -12,19 +12,45 @@
 
 alias NimbleCSV.RFC4180, as: CSV
 alias Llp.Repo
-alias Llp.Characters.Radical
+alias Llp.Characters
+alias Llp.Characters.{Radical, Kanji}
 
-"#{File.cwd!}/radicals.csv"
-|> File.read!
-|> CSV.parse_string
-|> Enum.each(fn [id, radical, stroke_count, onyomi, kunyomi, meaning] ->
-  %Radical{}
-  |> Radical.changeset(
-    %{
-      "radical" => radical, "stroke_count" => stroke_count,
-      "onyomi" => String.split(onyomi, ","),
-      "kunyomi" => String.split(kunyomi, ",")
-    }
-  )
-  |> Repo.insert!()
-end)
+defmodule Llp.CharacterSeed do
+  def add_radical([_id, radical, stroke_count, onyomi, kunyomi, _meaning]) do
+    %Radical{}
+      |> Radical.changeset(
+        %{
+          "radical" => radical, "stroke_count" => stroke_count,
+          "onyomi" => String.split(onyomi, ","),
+          "kunyomi" => String.split(kunyomi, ",")
+        }
+      )
+      |> Repo.insert!()
+  end
+
+  def add_kanji([_id, kanji, stroke_count, onyomi, kunyomi, meaning, radical]) do
+    radical
+      |> Characters.get_radical_by_character!
+      |> Ecto.build_assoc(:kanji)
+      |> Kanji.changeset(
+        %{
+          "kanji" => kanji, "stroke_count" => stroke_count,
+          "onyomi" => String.split(onyomi, ","),
+          "kunyomi" => String.split(kunyomi, ","),
+          "meaning" => String.split(meaning, ",")
+        }
+      )
+      |> Repo.insert!()
+  end
+end
+
+[
+  {"radicals", &Llp.CharacterSeed.add_radical/1},
+  {"kanji", &Llp.CharacterSeed.add_kanji/1}
+]
+  |> Enum.each(fn {str, fun} ->
+    "#{File.cwd!}/#{str}.csv"
+      |> File.read!
+      |> CSV.parse_string
+      |> Enum.each(fun)
+  end)
